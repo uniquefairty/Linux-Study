6a8dc8940dc5be5ba95b0adb1806f23acb4d31fe
uniquefarity <1985479524@qq.com>
2019-12-30 16:06:46 +0800

    静态库和动态库的生成和使用

---
+++ libraries/sub.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int sub(int a,int b);
+
+
---
+++ libraries/test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "sub.h"
+
+int main()
+{
+    int a=100;
+    int b=20;
+    printf("sub(a,b)=%d\n",sub(a,b));
+    return 0;
+}

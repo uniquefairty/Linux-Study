cb741adaf196b82554076b69cf8551f2c6a39e48
uniquefarity <1985479524@qq.com>
2020-04-14 10:43:11 +0800

    不同状态码的测试

--- http/httptest/svr.cpp
+++ http/httptest/svr.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <sstream>
 
 
 int main(int argc,char* argv[])
@@ -44,6 +45,7 @@ int main(int argc,char* argv[])
       peerts.Recv(buf);
       printf("chrome send Data:%s\n",buf.c_str());
   
+#if 0
       //http协议去组织数据格式
       //响应首行
       //响应头部
@@ -56,6 +58,49 @@ int main(int argc,char* argv[])
       std::string header;
       header.assign(arraybuf,strlen(arraybuf));
       //snprintf(buf,size,"%s%s%s",str,str,str);安全，但是不知道size大小
+#endif
+
+#if 0
+      //C++的方式实现
+      std::string body="<html><body><h1>hello world</h1></body></html>";
+      std::stringstream ss;//定义一个流对象
+      ss<<"HTTP/1.1 200 OK\r\n";
+      ss<<"Content-Length: "<<body.size()<<"\r\n";
+      ss<<"Content-Type: text/html\r\n";
+      ss<<"\r\n";
+      std::string header=ss.str();
+#endif
+
+#if 0
+      //302 状态码表示未找到  跳转到百度
+      std::string body="<html><body><h1>hello world</h1></body></html>";
+      std::stringstream ss;//定义一个流对象
+      ss<<"HTTP/1.1 302 Found\r\n";
+      ss<<"Content-Length: "<<body.size()<<"\r\n";
+      ss<<"Content-Type: text/html\r\n";
+      ss<<"Location:https://www.baidu.com\r\n";
+      ss<<"\r\n";
+#endif
+
+#if 0
+      //404 状态码 页面找不到
+      std::string body="<html><body><h1>hello world</h1></body></html>";
+      std::stringstream ss;//定义一个流对象
+      ss<<"HTTP/1.1 404 Page Not Found\r\n";
+      ss<<"Content-Length: "<<body.size()<<"\r\n";
+      ss<<"Content-Type: text/html\r\n";
+      ss<<"\r\n";
+#endif
+
+     //502 错误码 坏的网关 
+      std::string body="<html><body><h1>hello world</h1></body></html>";
+      std::stringstream ss;//定义一个流对象
+      ss<<"HTTP/1.1 502 Bad Gatway\r\n";
+      ss<<"Content-Length: "<<body.size()<<"\r\n";
+      ss<<"Content-Type: text/html\r\n";
+      ss<<"\r\n";
+
+      std::string header=ss.str();
       peerts.Send(header);
       peerts.Send(body);
 
